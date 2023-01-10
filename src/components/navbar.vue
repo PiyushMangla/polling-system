3536f5ef6800088269fe8748aae9f5462c500955
unknown <piyushmangla@innotechteam.in>
2023-01-10 11:50:10 +0530

    html and css for login and signin

---
+++ src/components/navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+    <h1 class="title">Polling System</h1>
+    <div class="home">
+      <router-link to="/">Home</router-link>
+    </div>
+    <div class="logins">
+      <router-link to="/login">Login</router-link>
+      <router-link to="/signin">Signin</router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "navBar",
+  setup() {},
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: () => import('../views/home.vue')
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/signin',
+    name: 'signin',
+    component: () => import('../views/signin.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/login.vue')
   }
 ]
 
